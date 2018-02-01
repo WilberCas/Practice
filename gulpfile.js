32ce2db638704cacfa5e8e7176892ad2eb7873c4
wcastaneda <wcastaneda@ztr.com>
2018-02-01 12:54:14 -0500

    Modified gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,6 @@ var gulp = require ('gulp');
 
 // Task: print hello world
 gulp.task('hello-world', function () {
+	console.log('Hello there!. It\'s a tets for gulp!!!');
 	console.log('hello world');
 })
